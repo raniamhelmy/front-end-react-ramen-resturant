aa68e66a88dbc70b4db945d54b6c0515d444a010
Rania-Helmy <ranmhelmy@gmail.com>
2021-12-01 15:21:16 +0300

    update paths

---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../App.css"
+import HeroSection from "../components/HeroSection"
+import Products from "../components/Products"
+import Desserts from "../components/Desserts"
+import Drinks from "../components/Drinks"
+import Speciality from '../components/Speciality'
+import Footer from '../components/Footer'
+import { ramenData, dessertData, drinksData } from '../components/data';
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Products heading={'Choose your Favorite'} data={ramenData}/>
+      <Speciality/>
+      <Desserts heading={'Sweet Treats for You'} data={dessertData}/>
+      <Drinks heading={'Beverages from Heaven'} data={drinksData}/>
+      <Footer/>
+    </>
+  );
+}
+
+export default Home;
